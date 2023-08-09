821651a04d1dbb1f3ee56c4ce72a7e2193b9d346
asus <asus@Host-Asus.homenet.telecomitalia.it>
2023-08-09 22:10:14 +0200

    Mongodb docker config

---
+++ init-kafka.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# blocks until kafka is reachable
+kafka-topics --bootstrap-server kafka:29092 --list
+
+echo -e 'Creating kafka topics'
+kafka-topics --bootstrap-server=kafka:29092 --create --if-not-exists --topic TRANSACTION_REGISTER --replication-factor 1 --partitions 1
+kafka-topics --bootstrap-server=kafka:29092 --create --if-not-exists --topic TRANSACTION_FAILED --replication-factor 1 --partitions 1
+kafka-topics --bootstrap-server=kafka:29092 --create --if-not-exists --topic TRANSACTION_SUCCESS --replication-factor 1 --partitions 1
+
+echo -e 'Successfully created the following topics:'
+kafka-topics --bootstrap-server kafka:29092 --list
